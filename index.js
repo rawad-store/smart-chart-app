129f01d5640b5ef3fdbac26f3e7dade54009eca9
مكتبة الرواد - الصوة <tarek500500@gmail.com>
2026-02-15 14:57:09 +0300

    Initialize React application with SmartChartAnalyzer

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SmartChartAnalyzer from './SmartChartAnalyzer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SmartChartAnalyzer />
+  </React.StrictMode>
+);
